d17d0fd0259171b88fe658751723be620fb0bd57
uCrover <chris66xd@gmail.com>
2022-02-07 16:22:23 -0500

    nueva organizacion
    x

--- customer-service/src/main/java/com/p1/users/project1NTTData/model/PersonaEntity.java
+++ customer-service/src/main/java/com/p1/users/project1NTTData/model/PersonaEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class PersonaEntity {
 
     @Id
-    private String id;
+    private String _id;
     private String nombre;
     private String apellido;
     private int edad;
